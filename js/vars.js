3fe24adee66eb6a6a8807e3fb271ec0e925420ac
Paul Thomas <paulthomas2107@gmail.com>
2023-10-16 11:21:06 +0100

    xxx

---
+++ js/vars.js
@@ -0,0 +1,20 @@
+// var, let, const
+
+// numeric
+let age;
+age = 21;
+let x = 22;
+age++;
+
+// string
+let firstName = 'Paul';
+
+// boolean
+let isAlive = true;
+
+console.log(age, x, firstName, isAlive, 'Hello ' + firstName);
+
+// Usinf HTML
+document.getElementById('p1').innerHTML = 'Hello ' + firstName;
+document.getElementById('p2').innerHTML = 'You are ' + age;
+document.getElementById('p3').innerHTML = 'Alive:  ' + isAlive;
